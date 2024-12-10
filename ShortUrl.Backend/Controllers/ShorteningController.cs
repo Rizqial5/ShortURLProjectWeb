b87189704df02a89f884a048f8b915dd1104cd98
Rizqial5 <bagusriski1@gmail.com>
2024-12-10 21:30:25 +0700

    Add Data package and ShortenData Model, COntroller template

---
+++ ShortUrl.Backend/Controllers/ShorteningController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortUrl.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShorteningController : ControllerBase
+    {
+    }
+}
