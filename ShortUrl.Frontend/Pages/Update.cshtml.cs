0d8e7821ae019bb50f5013ecba08aefadb3f6e29
Rizqial5 <bagusriski1@gmail.com>
2024-12-11 20:18:20 +0700

    Add OnGet and OnPost in Update.cshtml FrontEnd

--- ShortUrl.Frontend/Pages/Update.cshtml.cs
+++ ShortUrl.Frontend/Pages/Update.cshtml.cs
@@ -5,20 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ShortUrl.Backend.Models;
+using ShortUrl.Frontend.Services;
 
 namespace ShortUrl.Frontend.Pages
 {
     public class Update : PageModel
     {
         private readonly ILogger<Update> _logger;
+        private readonly IApiClient _apiClient;
 
-        public Update(ILogger<Update> logger)
+        [BindProperty]
+        public ShowUrl SelectedUrl {get;set;}
+
+        public Update(ILogger<Update> logger, IApiClient apiClient)
         {
             _logger = logger;
+            _apiClient = apiClient;
         }
 
-        public void OnGet()
+        public async Task OnGet(string shortCode)
         {
+            var sessions = await _apiClient.GetByShortAsync(shortCode);
+
+            SelectedUrl = sessions;
+        }
+
+        public async Task<IActionResult> OnPost( string inputUrl)
+        {
+        
+            await _apiClient.UpdateShortUrl(SelectedUrl!.ShortCode!, inputUrl);
+
+            return RedirectToPage("Index");
         }
     }
 }
